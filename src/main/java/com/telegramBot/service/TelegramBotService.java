1e794767174de7e6ddb49c67eff7e2bf2ff8e719
Kolomnin <kolomninsp@gmail.com>
2023-08-26 18:54:18 +0300

    Добавлены новые кнопки и реализован фенкционал отправки инструкции пользователю

--- src/main/java/com/telegramBot/service/TelegramBotService.java
+++ src/main/java/com/telegramBot/service/TelegramBotService.java
@@ -5,24 +5,22 @@ import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.telegramBot.entity.Icon;
-import liquibase.pro.packaged.S;
 import org.springframework.stereotype.Service;
 
-import static com.telegramBot.listener.TelegramBotUpdatesListener.CORP_DOC;
-import static com.telegramBot.listener.TelegramBotUpdatesListener.MENU_INSTRUCTION;
+import static com.telegramBot.entity.Commands.*;
 
 
 @Service
 public class TelegramBotService {
 
-    private final String header = Icon.TOPHAT.get() + "  Выбери нужный пункт меню:  " + Icon.TOPHAT.get();
+
     private final String buttonInstruction = Icon.CLIPBOARD.get() + "   Инструкции   " + Icon.CLIPBOARD.get();
     private final String buttonCorpDoc = Icon.DOC.get() + "   Корпоративные документы   " + Icon.DOC.get();
     private final String buttonUrlEmail = Icon.EMAIL.get() + "   Почта WEB   " + Icon.EMAIL.get();
     private final String buttonUrlItsm = Icon.SOS.get() + "   СОЗДАТЬ ЗАЯВКУ В ИТ   " + Icon.SOS.get();
 
-    private static final String urlEmail = "https://mail3.acron.ru";
-    private static final String urlItsm = "https://itsm.acron.ru/sd";
+    private  final String urlEmail = "https://mail3.acron.ru";
+    private  final String urlItsm = "https://itsm.acron.ru/sd";
 
     private final TelegramBot telegramBot;
 
@@ -35,12 +33,12 @@ public class TelegramBotService {
  */
     public void firstMenu(Long chatId) {
 
-        SendMessage helloMessage = new SendMessage(chatId, header);
+        SendMessage helloMessage = new SendMessage(chatId, HEADER.getValue());
 
         InlineKeyboardButton button1 = new InlineKeyboardButton(buttonInstruction);
-        button1.callbackData(MENU_INSTRUCTION);
+        button1.callbackData(MENU_INSTRUCTION.getValue());
         InlineKeyboardButton button2 = new InlineKeyboardButton(buttonCorpDoc);
-        button2.callbackData(CORP_DOC);
+        button2.callbackData(CORP_DOC.getValue());
         InlineKeyboardButton button3 = new InlineKeyboardButton(buttonUrlEmail);
         button3.url(urlEmail).callbackData();
         InlineKeyboardButton button4 = new InlineKeyboardButton(buttonUrlItsm);
@@ -56,17 +54,16 @@ public class TelegramBotService {
     }
 
     public void instructionMenu(Long chatId) {
-        SendMessage helloMessage = new SendMessage(chatId, "Выберите нужную инструкцию: ");
+        SendMessage helloMessage = new SendMessage(chatId, HEADER_MENU.getValue());
 
-        InlineKeyboardButton button1 = new InlineKeyboardButton("Инструкция 1");
-        button1.callbackData(MENU_INSTRUCTION);
-        InlineKeyboardButton button2 = new InlineKeyboardButton("📂 Корпоративные документы");
-        button2.callbackData(CORP_DOC);
-        InlineKeyboardButton button3 = new InlineKeyboardButton("Инструкция 3");
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Как сменить пароль?");
+        button1.callbackData(CHANGE_PASS.getValue());
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Настройка почты для iOS");
+        button2.callbackData(CORP_DOC.getValue());
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Настройка почты для Android");
         button3.url("https://mail3.acron.ru").callbackData();
-        InlineKeyboardButton button4 = new InlineKeyboardButton("Инструкция 4");
+        InlineKeyboardButton button4 = new InlineKeyboardButton("Как архивировать почту на ПК");
         button4.url("https://itsm.acron.ru/sd").callbackData();
-        InlineKeyboardButton button5 = new InlineKeyboardButton("Вернуться в Главное меню");
 
         InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
         keyboard.addRow(button1);
@@ -79,11 +76,33 @@ public class TelegramBotService {
         telegramBot.execute(helloMessage);
     }
 
-    /**   Необходимо создать метод для вызова: 1. меню с инструкциями;
+    public void changePassMenu(Long chatId) {
+        SendMessage helloMessage = new SendMessage(chatId, HEADER_MENU.getValue());
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Смена пароля на почте через браузер");
+        button1.callbackData(BROWSER.getValue());
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Смена пароля до его истечения");
+        button2.callbackData(CORP_DOC.getValue());
+        InlineKeyboardButton button3 = new InlineKeyboardButton("Изменение пароля для VPN-подключения");
+        button3.url("https://mail3.acron.ru").callbackData();
+
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.addRow(button1);
+        keyboard.addRow(button2);
+        keyboard.addRow(button3);
+
+
+
+        helloMessage.replyMarkup(keyboard);
+        telegramBot.execute(helloMessage);
+    }
+
+
+    /**   Необходимо создать метод для вызова: 1.
      *                                         2. Корпоративные док-ты;
-     *                                         3. Меню команд в боте;
+     *                                         3.
      *                                         4. выдача pdf файла с инструкциями;
-     *                                         5. Приветствие c именем кто пользуетсят ботом.
+     *                                         5.
      */
 
 }
--- src/main/java/com/telegramBot/service/TelegramMenuService.java
+++ src/main/java/com/telegramBot/service/TelegramMenuService.java
@@ -1,11 +1,15 @@
 package com.telegramBot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendDocument;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.telegramBot.entity.Icon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class TelegramMenuService {
@@ -31,8 +35,8 @@ public class TelegramMenuService {
     public void sendWelcomeMessage(Long chatId, String firstName) {
 
         String message = "Привет, " + firstName + "!  " + Icon.SMILE.get() + "\n" + "\nЯ  твой  " +
-                "IT-помощник.  Здесь  собран  \nсписок ответов  на  часто  задаваемые \nвопросы, чтобы" +
-                " ты мог быстрее найти \nдля  себя  нужную  информацию.";
+                         "IT-помощник.  Здесь  собран  \nсписок ответов  на  часто  задаваемые \nвопросы, чтобы" +
+                         " ты мог быстрее найти \nдля  себя  нужную  информацию.";
 
         telegramBot.execute(new SendMessage(chatId, message));
     }
@@ -40,19 +44,36 @@ public class TelegramMenuService {
     /**
      * Метод для пользователя, который впервые в нашем приюте.
      * Метод предлагает выбрать дальнейшие действия.
-     *
-     * @return
      */
 
 
-    public String getCorporationDoc() {
-        return "Здесь должны быть корпоративные документы или ссылки на них.";
+    public void getCorporationDoc(Long chatId) {
+
+        String messageDoc = "Здесь должны быть корпоративные документы или ссылки на них. Этап согласовывается.";
+
+        telegramBot.execute(new SendMessage(chatId, messageDoc));
     }
 
 
+    public void info(Long chatId) {
+        String messageInfo = "Если вы не нашли нужную инструкцию(обязательно скажите об этом нам и мы ее добавим)," +
+                             " или у вас возникли затруднения, вы всегда можете позвонить на общий номер компании," +
+                             " в группу технической поддержки т: +7(495)-745-70-47 вн.т: 5445 или написать нам по " +
+                             "адресу helpdesk@acron.ru";
 
-        public String getLinkTicketToIt () {
-            return "Здесь должен быть реализован функционал перехода по ссылке https://itsm.acron.ru/sd/operator";
-        }
-
+        telegramBot.execute(new SendMessage(chatId, messageInfo));
     }
+
+    public void changePass(Update update) {
+        logger.info("Запущен метод: changePass для пользователя с ID: " +
+                update.callbackQuery().message().chat().id());
+
+        String path = "src/main/resources/instruction/Change_pass.pdf";
+        File changePass = new File(path);
+        SendDocument sendDocument = new SendDocument(update.callbackQuery().message().chat().id(), changePass);
+        sendDocument.caption("В этом документе мы собрали ответы на самые важные вопросы по смене пароля");
+
+        // Выполнение запроса на отправку документа
+        telegramBot.execute(sendDocument);
+    }
+}
