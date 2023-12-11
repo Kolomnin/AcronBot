8b441bb14db0c933e6c9048d737a9bc1a242344e
Sergey <94751492+Kolomnin@users.noreply.github.com>
2023-12-11 23:17:00 +0300

    Update NotificationTask.java

--- src/main/java/com/telegramBot/entity/NotificationTask.java
+++ src/main/java/com/telegramBot/entity/NotificationTask.java
@@ -1,75 +1,75 @@
-package com.telegramBot.entity;
+// package com.telegramBot.entity;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Objects;
+// import javax.persistence.*;
+// import java.time.LocalDateTime;
+// import java.util.Objects;
 
-@Entity
-@Table(name = "tasks")
-public class NotificationTask {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-    @Column(name = "chat_id")
-    private Long chatId;
-    @Column(name = "date_time")
-    private LocalDateTime dateTime;
-    @Column(name = "description")
-    private String description;
+// @Entity
+// @Table(name = "tasks")
+// public class NotificationTask {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name = "id")
+//     private Long id;
+//     @Column(name = "chat_id")
+//     private Long chatId;
+//     @Column(name = "date_time")
+//     private LocalDateTime dateTime;
+//     @Column(name = "description")
+//     private String description;
 
-    public Long getChatId() {
-        return chatId;
-    }
+//     public Long getChatId() {
+//         return chatId;
+//     }
 
-    public void setChatId(Long chatId) {
-        this.chatId = chatId;
-    }
+//     public void setChatId(Long chatId) {
+//         this.chatId = chatId;
+//     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
+//     public LocalDateTime getDateTime() {
+//         return dateTime;
+//     }
 
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
+//     public void setDateTime(LocalDateTime dateTime) {
+//         this.dateTime = dateTime;
+//     }
 
-    public String getDescription() {
-        return description;
-    }
+//     public String getDescription() {
+//         return description;
+//     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//     public void setDescription(String description) {
+//         this.description = description;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        NotificationTask that = (NotificationTask) o;
-        return Objects.equals(id, that.id) && Objects.equals(chatId, that.chatId) && Objects.equals(dateTime, that.dateTime) && Objects.equals(description, that.description);
-    }
+//     @Override
+//     public boolean equals(Object o) {
+//         if (this == o) return true;
+//         if (o == null || getClass() != o.getClass()) return false;
+//         NotificationTask that = (NotificationTask) o;
+//         return Objects.equals(id, that.id) && Objects.equals(chatId, that.chatId) && Objects.equals(dateTime, that.dateTime) && Objects.equals(description, that.description);
+//     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, chatId, dateTime, description);
-    }
+//     @Override
+//     public int hashCode() {
+//         return Objects.hash(id, chatId, dateTime, description);
+//     }
 
-    @Override
-    public String toString() {
-        return "NotificationTask{" +
-                "id=" + id +
-                ", chatId=" + chatId +
-                ", dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                '}';
-    }
-}
+//     @Override
+//     public String toString() {
+//         return "NotificationTask{" +
+//                 "id=" + id +
+//                 ", chatId=" + chatId +
+//                 ", dateTime=" + dateTime +
+//                 ", description='" + description + '\'' +
+//                 '}';
+//     }
+// }
