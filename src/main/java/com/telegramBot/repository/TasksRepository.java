adc4516c09bc682bab5171eb1d7d84906c6e3c95
Sergey <94751492+Kolomnin@users.noreply.github.com>
2023-12-11 21:54:34 +0300

    Update TasksRepository.java

--- src/main/java/com/telegramBot/repository/TasksRepository.java
+++ src/main/java/com/telegramBot/repository/TasksRepository.java
@@ -1,13 +1,13 @@
-package com.telegramBot.repository;
+// package com.telegramBot.repository;
 
-import com.telegramBot.entity.NotificationTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import com.telegramBot.entity.NotificationTask;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
+// import java.time.LocalDateTime;
+// import java.util.Collection;
 
-@Repository
-public interface TasksRepository extends JpaRepository<NotificationTask, Long> {
-    Collection<NotificationTask> findAllByDateTime(LocalDateTime dateTime);
-}
+// @Repository
+// public interface TasksRepository extends JpaRepository<NotificationTask, Long> {
+//     Collection<NotificationTask> findAllByDateTime(LocalDateTime dateTime);
+// }
